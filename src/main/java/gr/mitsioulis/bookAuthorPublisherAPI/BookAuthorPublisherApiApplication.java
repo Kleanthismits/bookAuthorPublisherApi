c4e87982d48b6e1d75fd6d531fd41971b5da8a3a
Kleanthis Mitsioulis <kleanthismits@hotmail.gr>
2021-03-16 23:41:13 +0200

    Added Instructions in README file

--- src/main/java/gr/mitsioulis/bookAuthorPublisherAPI/BookAuthorPublisherApiApplication.java
+++ src/main/java/gr/mitsioulis/bookAuthorPublisherAPI/BookAuthorPublisherApiApplication.java
@@ -1,26 +1,12 @@
 package gr.mitsioulis.bookAuthorPublisherAPI;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import gr.mitsioulis.bookAuthorPublisherAPI.model.Author;
-import gr.mitsioulis.bookAuthorPublisherAPI.service.BookService;
 
 @SpringBootApplication
 public class BookAuthorPublisherApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookAuthorPublisherApiApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner run(BookService bookService) {
-		return (args) -> {
-			Author author = new Author();
-			author.setEmailAddress("auth@email.com");
-			author.setFirstName("George");
-		};
 	}
 }
